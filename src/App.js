f0da21117252f7dae3a192d6b86e6ccdfbcd0267
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-09-22 16:27:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+
 import Head from "./Components/Layout/Head";
 import Title from "./Components/Layout/Title";
 import logo from "./logo.svg";
 import Body from "./Components/Layout/Body";
 import Portal from "./Components/Portal/Portal";
 import { useState } from "react";
+import CartProvider from "./Components/Store/CartProvider";
+import CartContext from "./Components/Store/CartContext";
 
 function App() {
 
@@ -20,11 +23,12 @@ function App() {
   }
  
   return (
-    <>
+    <CartProvider>
     <Head openCart={openCart}/>
     {cartOpen && <Portal closeCart={closeCart} />}
-    <Body/>
-    </>
+    <Body></Body>
+    </CartProvider>
+    
   );
 }
 
