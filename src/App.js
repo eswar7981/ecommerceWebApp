715e57a07763e4ccfab5ae290c29dab79624cc1c
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-09-22 12:04:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import Head from "./Components/Layout/Head";
 import Title from "./Components/Layout/Title";
 import logo from "./logo.svg";
 import Body from "./Components/Layout/Body";
+import Portal from "./Components/Portal/Portal";
+import { useState } from "react";
 
 function App() {
+
+
+
+  const [cartOpen,setCartOpen]=useState(false)
+
+  const openCart=()=>{
+    setCartOpen(true)
+  }
+
+  const closeCart=()=>{
+    setCartOpen(false)
+  }
+ 
   return (
     <>
-      <div className="container" >
-        <Head></Head>
-          <Body></Body>
-        </div>
-
+    <Head openCart={openCart}/>
+    {cartOpen && <Portal closeCart={closeCart} />}
+    <Body/>
     </>
   );
 }
