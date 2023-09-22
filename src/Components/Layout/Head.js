63774b94293b50a428e039a310a7f34e31643c9f
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-09-22 12:08:06 +0530

    Update Head.js

--- src/Components/Layout/Head.js
+++ src/Components/Layout/Head.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Head.css'
-const Head = () => {
+const Head = (props) => {
   return (
  <>
  <div className='container'>
  <h1>head</h1>
+ <button onClick={props.openCart}>Cart</button>
  </div>
  </>
   )
